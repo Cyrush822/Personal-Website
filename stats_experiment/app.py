335be1dbbd0618ef3813a827298815e3220046af
Cyrush <cyrushung8222@gmail.com>
2021-04-23 11:08:42 +0800

    Moved files and testing python app

--- stats_experiment/app.py
+++ stats_experiment/app.py
@@ -1,13 +1,18 @@
-import os
-import sys
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 
 
-sys.path.insert(0, os.path.dirname(__file__))
+@app.route('/')
+def index():
+    return 'Hello, Flask!'
 
 
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python v' + sys.version.split()[0] + '\n'
-    response = '\n'.join([message, version])
-    return [response.encode()]
+@app.route('/bee')
+def bee():
+    return 'Flask is nice'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
